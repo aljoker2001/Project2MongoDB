fed540f043228f66db3cd5ea1dae716195aac9c8
aljoker2001 <aljoker2001@hotmail.com>
2019-05-27 10:11:37 -0700

    added body parser and got the new user added to the API list

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,19 +1,19 @@
-var express = require("express");
 var friends = require("../data/friends.js");
-
-var app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 module.exports = function (app) {
     app.get("/api/friends", function(req, res) {
         return res.json(friends);
     });
     
-    app.post("/api/friends", function(req, res) {
-        var user = req.body;
-        console.log(user);
-    })
+    app.post("/api/friends", function (req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var newFriend = req.body;
+        console.log("new friend:", newFriend);
+        friends.push(newFriend);
+    
+        // We then display the JSON to the users
+        res.json(friends);
+    });
 }
 
--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const path = require('path');
 var app = express();
-var allFriends = require('./app/data/friends')
+var allFriends = require('./app/data/friends');
+var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 require(path.join(__dirname, './app/routing/htmlRoutes.js'))(app);
 require(path.join(__dirname, "./app/routing/apiRoutes.js"))(app);
 
-app.post("/api/friends", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newFriend = req.body;
-    console.log("new friend:", newFriend);
-    allFriends.friends.push(newFriend);
+console.log(allFriends);
 
-    // We then display the JSON to the users
-    res.json(allFriends.friends);
-});
 
 app.listen(PORT, function() {
     console.log(`App is listening on port ${PORT}`);
