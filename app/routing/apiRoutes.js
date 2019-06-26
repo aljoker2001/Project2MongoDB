1e1372a88e3fea56de355a5b9a52ab39950cb548
aljoker2001 <aljoker2001@hotmail.com>
2019-06-25 17:11:46 -0700

    replaced MySQL with MongoDB

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,19 +1,36 @@
-var friends = require("../data/friends.js");
-
 module.exports = function (app) {
-    app.get("/api/friends", function(req, res) {
-        return res.json(friends);
-    });
-    
-    app.post("/api/friends", function (req, res) {
-        // req.body hosts is equal to the JSON post sent from the user
-        // This works because of the body parsing middleware
-        var newFriend = req.body;
-        console.log("new friend:", newFriend);
-        friends.push(newFriend);
-    
-        // The JSON is then displayed to the user
-        res.json(friends);
-    });
-}
+  var mongojs = require('mongojs')
+  var databaseUrl = 'frienddb'
+  var collections = ['friends']
 
+  // Use mongojs to hook the database to the db variable
+  var db = mongojs(databaseUrl, collections)
+
+  // This makes sure that any errors are logged if mongodb runs into an issue
+  db.on('error', function (error) {
+    console.log('Database Error:', error)
+  })
+  app.get('/api/friends', function (req, res) {
+    db.friends.find({}, function (err, data) {
+      if (err) {
+        console.log(err)
+      } else {
+        res.json(data)
+      }
+    })
+  })
+
+  app.post('/api/friends', function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of the body parsing middleware
+    var newFriend = req.body
+    console.log('new friend:', newFriend)
+    db.friends.insert(newFriend, function (err, data) {
+      if (err) {
+        console.log(err)
+      } else {
+        res.json(data)
+      }
+    })
+  })
+}
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const path = require('path');
-var app = express();
+const express = require('express')
+const path = require('path')
+var app = express()
 // var allFriends = require('./app/data/friends');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000
 
-// The body parser package helps verify user input 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// The body parser package helps verify user input
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // These requires take the get routes defined in htmlRoutes and apiRoutes
-require(path.join(__dirname, './app/routing/htmlRoutes.js'))(app);
-require(path.join(__dirname, "./app/routing/apiRoutes.js"))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes.js'))(app)
+require(path.join(__dirname, './app/routing/apiRoutes.js'))(app)
 
-app.listen(PORT, function() {
-    console.log(`App is listening on port ${PORT}`);
+app.listen(PORT, function () {
+  console.log(`App is listening on port ${PORT}`)
 })
